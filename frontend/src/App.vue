b3c0ed3e2ae059d3c6af3552e920fbc7a36c867f
Serebr1k-code <Serebr11k@gmail.com>
2026-06-04 19:17:45 +0300

    Apply theme on app startup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,10 +5,13 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
 import { useAuthStore } from './stores/auth'
+import { useThemeStore } from './stores/theme'
 
 const authStore = useAuthStore()
+const themeStore = useThemeStore()
 
 onMounted(() => {
   authStore.init()
+  themeStore.applyTheme(themeStore.currentTheme)
 })
 </script>
